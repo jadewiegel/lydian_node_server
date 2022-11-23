fc5d6ec808383d09436af28330da423c6216fcc0
Jade Wiegel <jadewiegel@gmail.com>
2022-11-22 20:58:34 -0600

    more server notes in node

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const port = 5001;
 
 app.use(express.static('server/public'));
 
+const quoteList = require('./modules/quoteList');
+
 app.listen(port, () => {
     console.log('listening on port, ', port);
 })
@@ -14,11 +16,12 @@ app.get('/quotes', function(req, res) {
     res.send(quoteList);
 } );
 
-let quoteList = [
-    { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
-    { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
-    { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }    
-];
+
 
 
 // control + c ---- will close the server
+
+// C - create or post
+// R - Read or get 
+// U - Update or put
+// D - Delete 
